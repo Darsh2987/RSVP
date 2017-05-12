1b99484eea69bc60141148be217e969849121fb2
Darsh <darsh2987@gmail.com>
2017-05-12 11:23:44 +0100

    application completed - added edit and filter buttons with functionality.

--- app.js
+++ app.js
@@ -2,12 +2,46 @@
 // Declaring variables.
 const form = document.querySelector('#registrar');
 const input = form.querySelector('input');
+
+const mainDiv = document.querySelector('.main');
 const ul = document.querySelector('#invitedList');
+
+const div = document.createElement('div');
+const filterLabel = document.createElement('label');
+const filterCheckBox = document.createElement('input');
+
+filterLabel.textContent = "Hide those who haven't responded";
+filterCheckBox.type = 'checkbox';
+div.appendChild(filterLabel);
+div.appendChild(filterCheckBox);
+mainDiv.insertBefore(div, ul);
+
+filterCheckBox.addEventListener('change', (e) => {
+  const isChecked = e.target.checked;
+  const lis = ul.children;
+  if(isChecked) {
+    for (let i = 0; i < lis.length; i += 1) {
+      let li = lis[i];
+      if (li.className === 'responded') {
+        li.style.display = '';
+      } else {
+        li.style.display = 'none';
+      }
+    }
+  } else {
+    for (let i = 0; i < lis.length; i += 1) {
+      let li = lis[i];
+      li.style.display = '';
+    }
+  }
+});
 
 // Function to create list item.
 function createLi(text) { // Create function "createLi" with a  argument of the "text".
   const li = document.createElement('li'); // Create list item and store it in the variable "li".
-  li.textContent = text; // Text content of list item("li") will be equal to the value of the input value stored within the variable "text".
+
+  const span = document.createElement('span'); // Create an input element and store it in the variable "checkbox".
+  span.textContent = text; // Text content of list item("li") will be equal to the value of the input value stored within the variable "text".
 
   const label = document.createElement('label'); // Create label for the list item and store it in the variable "label".
   label.textContent = 'Confirmed'; // Text content of the label is equal to "confirmed".
@@ -15,12 +49,17 @@ function createLi(text) { // Create function "createLi" with a  argument of the 
   const checkbox = document.createElement('input'); // Create an input element and store it in the variable "checkbox".
   checkbox.type = 'checkbox'; // Setting the input element type to a checkbox type.
 
-  const button = document.createElement('button'); // Create a button element and store it in the variable "button".
-  button.textContent = 'remove'; // Text content of the button will be equal to "remove".
+  const editButton = document.createElement('button'); // Create a "edit" button element and store it in the variable "button".
+  editButton.textContent = 'edit'; // Text content of the button will be equal to "edit".
 
+  const removeButton = document.createElement('button'); // Create a "remove" button element and store it in the variable "button".
+  removeButton.textContent = 'remove'; // Text content of the button will be equal to "remove".
+
+  li.appendChild(span);
   label.appendChild(checkbox); // Append the checkbox element to the label element.
   li.appendChild(label); // Append the label element to the li element.
-  li.appendChild(button); // Append the button element to the li element.
+  li.appendChild(editButton); // Append the edit button element to the li element.
+  li.appendChild(removeButton); // Append the remove button element to the li element.
   return li; // Return the li element.
 }
 
@@ -47,8 +86,26 @@ ul.addEventListener('change', (e) => {
 
 ul.addEventListener('click', (e) => {
   if (e.target.tagName === 'BUTTON') {
-    const li = e.target.parentNode;
+    const button = e.target;
+    const li = button.parentNode;
     const ul = li.parentNode;
-    ul.removeChild(li);
+    if (button.textContent === 'remove') {
+      ul.removeChild(li);
+    } else if (button.textContent === 'edit') {
+      const span = li.firstElementChild;
+      const input = document.createElement('input');
+      input.type = 'text';
+      input.value = span.textContent;
+      li.insertBefore(input, span);
+      li.removeChild(span);
+      button.textContent = 'save';
+    } else if (button.textContent === 'save') {
+      const input = li.firstElementChild;
+      const span = document.createElement('span');
+      span.textContent = input.value;
+      li.insertBefore(span, input);
+      li.removeChild(input);
+      button.textContent = 'edit';
+    }
   }
 });
